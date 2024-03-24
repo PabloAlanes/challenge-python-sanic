9d21fe781e6f0189dee7586b43871e5b5bb97fe3
pablo alanes <alanespablo@gmail.com>
2024-03-24 16:00:11 -0300

    fix: :wrench: Fix environment variables for RabbitMQ

--- transaction-service/settings/rabbitmq.py
+++ transaction-service/settings/rabbitmq.py
@@ -2,6 +2,6 @@ import os
 
 RABBITMQ_HOST = os.getenv('RABBITMQ_HOST') or '0.0.0.0'
 RABBITMQ_USER = os.getenv('RABBITMQ_DEFAULT_USER') or 'guest'
-RABBITMQ_PASS = os.getenv('RABBITMQ_DEFAULT_USER') or 'guest'
+RABBITMQ_PASS = os.getenv('RABBITMQ_DEFAULT_PASS') or 'guest'
 RABBITMQ_QUEUE = os.getenv('RABBITMQ_QUEUE') or 'task_queue'
 RABBITMQ_ENABLED = os.getenv('RABBITMQ_ENABLED') in ['true', 'True'] or False
