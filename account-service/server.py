0fee160e3bd17b66d2b3fe504ee7de6479050626
pablo alanes <alanespablo@gmail.com>
2024-03-24 10:58:47 -0300

    refactor: :art: Move sanic app to api module from server script

--- account-service/server.py
+++ account-service/server.py
@@ -1,17 +1,10 @@
-from sanic import Sanic
-from sanic.response import text
 from mongoengine import connect
+from account_service.api.app import app
 from settings.app import *
 from settings.mongodb import *
 
 # MongoDB connection
 connect(MONGODB_NAME, host=MONGODB_HOST, port=int(MONGODB_PORT))
-
-app = Sanic("MyHelloWorldApp")
-
-@app.get("/")
-async def hello_world(request):
-    return text("Hello, world.")
 
 if __name__ == '__main__':
     app.run(host=SANIC_APP_HOST, port=int(SANIC_APP_PORT), debug=DEBUG, auto_reload=DEBUG)
