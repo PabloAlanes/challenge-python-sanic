435259cdcbc2fcba0443f5de43003ea438de6633
pablo alanes <alanespablo@gmail.com>
2024-03-24 12:23:04 -0300

    feat: :sparkles: Add route for transaction view

--- transaction-service/transaction_service/api/app.py
+++ transaction-service/transaction_service/api/app.py
@@ -1,4 +1,6 @@
 from sanic import Sanic
+from transaction_service.api.views.transaction import TransactionView
 
 # Sanic setup
 app = Sanic("TransactionService")
+app.add_route(TransactionView.as_view(), "/transactions/<id:strorempty>/")
